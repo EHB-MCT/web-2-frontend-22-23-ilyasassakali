7ed77ad5f4d4f3d0a42ce8012a8123184d79211d
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2022-11-29 15:02:55 +0100

    searchtrackandalbumscriptadded

--- docs/js/search.js
+++ docs/js/search.js
@@ -3,8 +3,6 @@
 window.onload = function () {
     searchOnName();
 
-
-
     function searchOnName() {
         let searched = document.getElementById('searchbar');
         searched.addEventListener("keypress", function (event) {
@@ -13,61 +11,56 @@ window.onload = function () {
             }
             let searchword = searched.value.toLowerCase()
 
-            //search album
-            DZ.api(`/search/album?q=${searchword}`, function (response) {
-                console.log("search on: ", searchword);
-                console.log("fetch:", response.data);
-                let htmlString = "";
-                response.data.slice(0, 25).forEach(item => {
-                    //console.log("here are your items: ", item);
-                    const coverImage = item.cover_medium
-                    const title = item.title
-                    const artist = item.artist.name
-                    //console.log("c ca", artist);
-                    htmlString += ` <div class="elementContainer">
-                         <a href=""> <img src="${coverImage}" alt=""></a>
-                         <p>${title}</p>
-                         <p class="secondp">${artist}</p>
-                     </div>`
 
-                    document.getElementById("showContainer").innerHTML = htmlString;
+            if (document.getElementById('basicProgram').checked) {
+                // Basic package is checked
+                console.log("le premier");
+                DZ.api(`/search/track?q=${searchword}`, function (response) {
+                    //console.log("search on: ", searchword);
+                    console.log("fetch:", response.data);
+                    let htmlString = "";
+                    response.data.slice(0, 25).forEach(item => {
+                        //console.log("here are your items: ", item);
+                        const coverImage = item.album.cover_medium
+                        const title = item.title
+                        const artist = item.artist.name
+                        //console.log("c ca", artist);
+                        htmlString += ` <div class="elementContainer">
+                            <a href=""> <img src="${coverImage}" alt=""></a>
+                            <p>${title}</p>
+                            <p class="secondp">${artist}</p>
+                        </div>`
 
-                });
+                        document.getElementById("showContainer").innerHTML = htmlString;
 
+                    });
+                })
 
-            })
+            } else if (document.getElementById('proProgram').checked) {
+                // Pro package is checked
+                console.log("le deuxieme");
+                //search album
+                DZ.api(`/search/album?q=${searchword}`, function (response) {
+                    console.log("search on: ", searchword);
+                    console.log("fetch:", response.data);
+                    let htmlString = "";
+                    response.data.slice(0, 25).forEach(item => {
+                        //console.log("here are your items: ", item);
+                        const coverImage = item.cover_medium
+                        const title = item.title
+                        const artist = item.artist.name
+                        //console.log("c ca", artist);
+                        htmlString += ` <div class="elementContainer">
+                 <a href=""> <img src="${coverImage}" alt=""></a>
+                 <p>${title}</p>
+                 <p class="secondp">${artist}</p>
+             </div>`
 
+                        document.getElementById("showContainer").innerHTML = htmlString;
+                    });
+                })
 
-
-
-
+            }
         })
-
     };
-
-
-
 }
-
-//search track
-/*
-            DZ.api(`/search/track?q=${searchword}`, function (response) {
-                //console.log("search on: ", searchword);
-                console.log("fetch:", response.data);
-                let htmlString = "";
-                response.data.slice(0, 25).forEach(item => {
-                    //console.log("here are your items: ", item);
-                    const coverImage = item.album.cover_medium
-                    const title = item.title
-                    const artist = item.artist.name
-                    //console.log("c ca", artist);
-                    htmlString += ` <div class="elementContainer">
-                        <a href=""> <img src="${coverImage}" alt=""></a>
-                        <p>${title}</p>
-                        <p class="secondp">${artist}</p>
-                    </div>`
-
-                    document.getElementById("showContainer").innerHTML = htmlString;
-
-                });
-*/
