65e5796f015e9e78fbd35a05a0c9669daf958d31
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2022-12-09 02:13:09 +0100

    redirectionsAndCSS

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -2,7 +2,7 @@
 let user = JSON.parse(sessionStorage.getItem('user'))
 
 if (user) {
-    document.getElementById('welcomemessage').innerText = `Welcome ${user.username}!`
+    document.getElementById('welcomemessage').innerText = `Welcome ${user.username} !`
 } else {
     window.location.href = "./index.html"
 }
