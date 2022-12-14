972f30aebdf28d24b0c9f8ffb91e626f8258a168
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2022-12-14 18:57:28 +0100

    muzzAnAlbum

--- docs/js/domuzz.js
+++ docs/js/domuzz.js
@@ -64,12 +64,12 @@ window.onload = function () {
                     <h3>A score out of 100</h3>
                     <input id="w3review2" class="w3review2" type="number" max="100" min="0">
                     <br>
-                    <input id="moreinfobtn" class="moreinfobtn" type="submit" value="SAVE">
+                    <input id="moreinfobtn" class="moreinfobtn" type="submit" value="PUBLISH">
                 </form>`
 
                 document.getElementById("showContainer").innerHTML = htmlString;
 
-                //save muzzy and redirect to all muzzypage
+                //save track as muzzy and redirect to all muzzypage
                 const savebtn = document.getElementById('moreinfobtn')
                 savebtn.addEventListener('click', (event) => {
                     event.preventDefault();
@@ -94,14 +94,116 @@ window.onload = function () {
                     muzzy.time = time
 
                     //muzzy.username = user.username
-                    console.log("kaka", muzzy.username);
-
-
+                    //console.log("kaka", muzzy.username);
                     if (muzzy.opinion && muzzy.score) {
                         //add  muzzy in db
                         getData("http://localhost:3000/savemuzzy", "POST", muzzy).then(data => {
                             alert(data.message)
-                            //window.location.href = `./muzzy.html`
+                            window.location.href = `./muzzy.html`
+                        })
+                        async function getData(url, method, data) {
+                            let resp = await fetch(url, {
+                                method: method,
+                                headers: {
+                                    'Content-Type': "application/json"
+                                },
+                                body: JSON.stringify(data)
+                            });
+                            return await resp.json();
+                        }
+                    } else {
+                        alert("Some fields are missing: opinion, score")
+                    }
+                })
+            })
+        } else if (albumID) {
+            //fetch moreinfo about albums
+            DZ.api(`/album/${albumID}`, function (response) {
+                //console.log("fetch:", response);
+
+                let htmlString = "";
+
+                const coverImage = response.cover_big
+                const title = response.title
+                const artist = response.artist.picture_medium
+                const artistname = response.artist.name
+                const type = response.type
+                const date = response.release_date
+                const numberTracks = response.nb_tracks
+
+                let duration = response.duration
+                //https://bobbyhadz.com/blog/javascript-convert-seconds-to-minutes-and-seconds
+                const minutes = Math.floor(duration / 60);
+                const seconds = duration % 60;
+
+                function padTo2Digits(num) {
+                    return num.toString().padStart(2, '0');
+                }
+                const durationmin = `${padTo2Digits(minutes)}min ${padTo2Digits(seconds)}sec`;
+
+                //console.log(numberTracks);
+
+                htmlString += ` <div class="elementContainer">
+                <div >
+                 <img id="bigimg" class="bigimg" src="${coverImage}" alt="">
+                 </div>
+             <div id="bigtitle" class='bigtitle'>
+                 <h2>${title}</h2> 
+                 <div class="artsec">
+                 <a href="" ><img class="smallimg" src="${artist}" alt=""></a>
+                 <p id="artp" class="artp">${artistname}</p>      
+             </div>
+             <div class="textsection">
+             <p>Type: ${type}</p>
+             <p>Release Date: ${date}</p>
+             <p>Duration: ${durationmin}</p>
+             <p>Number of Tracks: ${numberTracks}</p>
+             </div> 
+              </div>
+           </div>
+           <form>
+                    <h3>Share your opinion and feelings here</h3>
+                    <textarea class="w3review" id="w3review"  rows="4"
+                        cols="50">Write here.</textarea>
+                    <h3>A score out of 100</h3>
+                    <input id="w3review2" class="w3review2" type="number" max="100" min="0">
+                    <br>
+                    <input id="moreinfobtn" class="moreinfobtn" type="submit" value="PUBLISH">
+                </form>`
+
+                document.getElementById("showContainer").innerHTML = htmlString;
+
+                //save album as muzzy and redirect to all muzzypage
+                const savebtn = document.getElementById('moreinfobtn')
+                savebtn.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    console.log("ta cliquez");
+
+                    //https://tecadmin.net/get-current-date-time-javascript/
+                    let today = new Date();
+                    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                    let time = today.getHours() + ":" + today.getMinutes();
+
+                    let user = JSON.parse(sessionStorage.getItem('user'))
+                    let muzzy = {}
+                    muzzy.muzzyimg = document.getElementById('bigimg').src;
+                    muzzy.muzzyalbum = document.getElementById('bigtitle').firstElementChild.innerHTML;
+                    muzzy.muzzyartist = document.getElementById('artp').innerHTML;
+                    muzzy.opinion = document.getElementById('w3review').value;
+                    muzzy.score = document.getElementById('w3review2').value;
+                    muzzy.username = user.username
+                    muzzy.date = date
+                    muzzy.time = time
+
+
+                    //console.log("go", muzzy.username);
+
+
+                    if (muzzy.opinion && muzzy.score) {
+                        //add  muzzy in db
+                        getData("http://localhost:3000/savealbummuzzy", "POST", muzzy).then(data => {
+                            alert(data.message)
+                            window.location.href = `./muzzy.html`
                         })
                         async function getData(url, method, data) {
                             let resp = await fetch(url, {
@@ -141,64 +243,6 @@ window.onload = function () {
 
 
 
-
-            })
-        } else if (albumID) {
-            //fetch moreinfo about albums
-            DZ.api(`/album/${albumID}`, function (response) {
-                //console.log("fetch:", response);
-
-                let htmlString = "";
-
-                const coverImage = response.cover_big
-                const title = response.title
-                const artist = response.artist.picture_medium
-                const artistname = response.artist.name
-                const type = response.type
-                const date = response.release_date
-                const numberTracks = response.nb_tracks
-
-                let duration = response.duration
-                //https://bobbyhadz.com/blog/javascript-convert-seconds-to-minutes-and-seconds
-                const minutes = Math.floor(duration / 60);
-                const seconds = duration % 60;
-
-                function padTo2Digits(num) {
-                    return num.toString().padStart(2, '0');
-                }
-                const durationmin = `${padTo2Digits(minutes)}min ${padTo2Digits(seconds)}sec`;
-
-                //console.log(numberTracks);
-
-                htmlString += ` <div class="elementContainer">
-                <div >
-                 <img class="bigimg" src="${coverImage}" alt="">
-                 </div>
-             <div class='bigtitle'>
-                 <h2>${title}</h2> 
-                 <div class="artsec">
-                 <a href="" ><img class="smallimg" src="${artist}" alt=""></a>
-                 <p class="artp">${artistname}</p>      
-             </div>
-             <div class="textsection">
-             <p>Type: ${type}</p>
-             <p>Release Date: ${date}</p>
-             <p>Duration: ${durationmin}</p>
-             <p>Number of Tracks: ${numberTracks}</p>
-             </div> 
-              </div>
-           </div>
-           <form>
-                    <h3>Share your opinion and feelings here</h3>
-                    <textarea class="w3review" id="w3review"  rows="4"
-                        cols="50">Write here.</textarea>
-                    <h3>A score out of 100</h3>
-                    <input class="w3review2" type="number" max="100" min="0">
-                    <br>
-                    <input id="moreinfobtn" class="moreinfobtn" type="submit" value="SAVE">
-                </form>`
-
-                document.getElementById("showContainer").innerHTML = htmlString;
 
 
             })
@@ -241,7 +285,7 @@ window.onload = function () {
                     <h3>A score out of 100</h3>
                     <input class="w3review2" type="number" max="100" min="0">
                     <br>
-                    <input id="moreinfobtn" class="moreinfobtn" type="submit" value="SAVE">
+                    <input id="moreinfobtn" class="moreinfobtn" type="submit" value="PUBLISH">
                 </form>`
 
                 document.getElementById("showContainer").innerHTML = htmlString;
--- docs/js/muzzy2.js
+++ docs/js/muzzy2.js
@@ -1,12 +1,15 @@
 window.onload = function () {
     //console.log("okee");
 
-    refreshmuzzypage()
+    refreshmuzzytracks()
 
-    function refreshmuzzypage() {
+
+
+    //does a get for having the last muzzy tracks on the page
+    async function refreshmuzzytracks() {
 
         let muzzy = sessionStorage.getItem("muzzy");
-        getData("http://localhost:3000/AllMuzzys", "GET", muzzy).then(data => {
+        await getData("http://localhost:3000/AllMuzzys", "GET", muzzy).then(data => {
             console.log("conva data:", data);
 
             let htmlString = ""
@@ -32,6 +35,51 @@ window.onload = function () {
 
             console.log("ca c ton string:", htmlString);
             document.getElementById("muzzelement").innerHTML = htmlString;
+
+        })
+        async function getData(url, method) {
+            let resp = await fetch(url, {
+                method: method,
+                headers: {
+                    'Content-Type': "application/json"
+                },
+            });
+            return await resp.json();
+        }
+        refreshmuzzyalbums()
+
+    }
+
+    //does a get for having the last muzzy albums on the page
+    async function refreshmuzzyalbums() {
+
+        let muzzy = sessionStorage.getItem("muzzy");
+        await getData("http://localhost:3000/AllalbumMuzzys", "GET", muzzy).then(data => {
+            console.log("conva data:", data);
+
+            let htmlString = ""
+            for (let r = data.length - 1; r >= 0; r--) {
+
+                htmlString += `
+                           <div class="muzzinner">
+                               <div class="inimg" id="inimg" >
+                                   <img id="inimg" src="${data[r].muzzyimg}">
+                               </div>
+                               <div class="text">
+                                   <h3 class="inp3" id="inp3">Album: ${data[r].muzzyalbum} - Artist: ${data[r].muzzyartist}</h3>
+                                   <p class="inp4" id="inp4">Opinion: ${data[r].opinion}</p>
+                                   <p class="inp2" id="inp2">Score: ${data[r].score}/100</p>
+                               </div>
+                               <div class="addedbby">
+                                   <p id="inp5">added by:<br> ${data[r].username}</p>
+                                   <p id="inp5">added date:<br> ${data[r].date}<br> ${data[r].time}</p>
+                               </div>
+                           </div>
+                                   `
+            }
+
+            console.log("ca c ton string:", htmlString);
+            document.getElementById("muzzelement2").innerHTML = htmlString;
 
         })
         async function getData(url, method) {
